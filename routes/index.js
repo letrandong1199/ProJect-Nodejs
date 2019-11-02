87ffa474217a1ddaf941fb889629247494755a38
letrandong1199 <49052236+letrandong1199@users.noreply.github.com>
2019-11-03 00:55:23 +0700

    Routing all html

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/index.html', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/women.html', function(req, res, next) {
+  res.render('women');
+});
+router.get('/buy.html', function(req, res, next) {
+  res.render('buy');
+});
+router.get('/contact.html', function(req, res, next) {
+  res.render('contact');
+});
+router.get('/brands.html', function(req, res, next) {
+  res.render('brands');
+});
+router.get('/details.html', function(req, res, next) {
+  res.render('details');
+});
+router.get('/registration.html', function(req, res, next) {
+  res.render('registration');
+});
+router.get('/men.html', function(req, res, next) {
+  res.render('women');
+});
+router.get('/kid.html', function(req, res, next) {
+  res.render('women');
+});
+router.get('/collection.html', function(req, res, next) {
+  res.render('women');
 });
 
 module.exports = router;
